d2b0279f68c466659ba70b8f1b31b86405d92ee9
Felipe-CdS <felipe32santos@hotmail.com>
2024-02-26 12:42:26 -0300

    mobile menu done

---
+++ index.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    let menu = document.getElementById("mobile-menu");
+
+    if (menu.style.display == "flex") {
+        document.getElementById("mobile-menu").style.display = "none";
+        return;
+    }
+    document.getElementById("mobile-menu").style.display = "flex";
+}
+
+document
+    .getElementById("mobile-menu-btn")
+    .addEventListener("click", toggleMenu);

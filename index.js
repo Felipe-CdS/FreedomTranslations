763f5b0be6e7090d8dc5a9975183cc8f8ef8433b
Felipe-CdS <felipe32santos@hotmail.com>
2024-03-01 15:03:42 -0300

    notebook backup

--- index.js
+++ index.js
@@ -13,3 +13,26 @@ document.getElementById("mobile-menu").style.display = "none";
 document
     .getElementById("mobile-menu-btn")
     .addEventListener("click", toggleMenu);
+
+// ######################################
+const emailServiceID = "service_wrah3aq";
+const emailTemplateID = "template_i31zycr";
+
+emailjs.init({ publicKey: "YOUR_PUBLIC_KEY" });
+
+window.onload = function() {
+    document
+        .getElementById("contact-form")
+        .addEventListener("submit", function(event) {
+            event.preventDefault();
+            // these IDs from the previous steps
+            emailjs.sendForm(emailServiceID, emailTemplateID, this).then(
+                () => {
+                    console.log("SUCCESS!");
+                },
+                (error) => {
+                    console.log("FAILED...", error);
+                },
+            );
+        });
+};
